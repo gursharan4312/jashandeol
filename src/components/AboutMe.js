e540120c41c26e14835e84a3c621ae202a8fbd72
Gursharan Singh <gursharansingh4312@gmail.com>
2020-11-15 06:18:15 -0800

    about me section change

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
+import Loader from "./Loader";
 
 function AboutMe() {
+  const [profile, setProfile] = useState({
+    header: "",
+    details: "",
+    image: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getProfile() {
+      setLoading(true);
+      let data = await fetch("admin/data/profile.json");
+      data = await data.json();
+      setProfile({
+        header: data.header,
+        details: data.details,
+        image: data.image,
+      });
+      setLoading(false);
+    }
+    getProfile();
+  }, []);
+
   return (
     <Container className="aboutme-container py-4">
-      <Row className="py-4 my-4">
-        <Col md={4}>
-          <img
-            src="/assests/images/profile.jpg"
-            alt="jashandeol"
-            className="img-fluid"
-          />
-        </Col>
-        <Col>
-          <h1>Hi there, my name is Jashandeol!</h1>
-          <p>
-            I'm a professional photographer and filmmaker located in
-            vancouver,BC
-          </p>
-        </Col>
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Row className="py-4 my-4">
+          <Col md={4}>
+            <img src={profile.image} alt="jashandeol" className="img-fluid" />
+          </Col>
+          <Col>
+            <h1>{profile.header}</h1>
+            <p>{profile.details}</p>
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 }
